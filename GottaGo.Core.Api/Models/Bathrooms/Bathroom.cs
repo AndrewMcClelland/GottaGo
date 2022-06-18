f6f84727886519de2180b7104e22c09404f99a58
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-18 10:34:15 -0700

    DATA: Initialize Bathroom Model with Submodels
    Closes #16

---
+++ GottaGo.Core.Api/Models/Bathrooms/Bathroom.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+using System;
+
+namespace GottaGo.Core.Api.Models.Bathrooms
+{
+    public class Bathroom
+    {
+        public Guid Id { get; set; }
+        public BathroomAddress Address { get; set; }
+        public BathroomCategory Category { get; set; }
+        public BathroomVerificationStatus VerificationStatus { get; set; }
+        public double Rating { get; set; }
+        public string EntryCode { get; set; }
+        public string Details { get; set; }
+        public DateTimeOffset CreatedDateTime { get; set; }
+        public DateTimeOffset LastUpdatedDateTime { get; set; }
+    }
+}
