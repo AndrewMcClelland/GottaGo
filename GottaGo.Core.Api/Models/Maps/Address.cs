4e7d8f69647ed20905e678b1988ea1328f30a3c8
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-20 17:53:16 -0700

    DATA: Create Address Model

---
+++ GottaGo.Core.Api/Models/Maps/Address.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+namespace GottaGo.Core.Api.Models.Maps
+{
+    public class Address
+    {
+        public string Type { get; set; }
+        public string StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string MunicipalitySubdivision { get; set; }
+        public string Municipality { get; set; }
+        public string CountrySubdivisionName { get; set; }
+        public string CountrySubdivision { get; set; }
+        public string CountrySecondarySubdivision { get; set; }
+        public string CountryTertiarySubdivision { get; set; }
+        public string PostalCode { get; set; }
+        public string ExtendedPostalCode { get; set; }
+        public string Country { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryCodeISO3 { get; set; }
+        public string FreeformAddress { get; set; }
+        public Coordinates Coordinates { get; set; }
+        public BoundingBox BoundingBox { get; set; }
+    }
+}
---
+++ GottaGo.Core.Api/Models/Maps/BoundingBox.cs
@@ -0,0 +1,12 @@
+﻿// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+namespace GottaGo.Core.Api.Models.Maps
+{
+    public class BoundingBox
+    {
+        public Coordinates TopLeftPoint { get; set; }
+        public Coordinates BottomRightPoint { get; set; }
+    }
+}
