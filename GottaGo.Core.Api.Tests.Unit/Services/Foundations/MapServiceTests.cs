57f9629c73b7a328485a6e566ff36b9780011bde
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-20 17:54:08 -0700

    FOUNDATIONS: MapServiceTests WIP

---
+++ GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+using GottaGo.Core.Api.Brokers.Loggings;
+using GottaGo.Core.Api.Brokers.MapApis;
+using Moq;
+
+namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
+{
+    public partial class MapServiceTests
+    {
+        private readonly Mock<IMapApiBroker> mapApiBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+    }
+}
