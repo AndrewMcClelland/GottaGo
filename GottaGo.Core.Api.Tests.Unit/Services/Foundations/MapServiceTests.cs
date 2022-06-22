d65a0dcbcb3b2f0330daf25672f7d7ad4bf32f07
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-21 22:22:40 -0700

    UNIT: MapServiceTests WIP

--- GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.cs
+++ GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.cs
@@ -2,9 +2,13 @@
 // Copyright (c) Andrew McClelland.
 // -----------------------------------
 
+using System;
 using GottaGo.Core.Api.Brokers.Loggings;
 using GottaGo.Core.Api.Brokers.MapApis;
+using GottaGo.Core.Api.Models.Externals.Maps.Search;
+using GottaGo.Core.Api.Services.Foundations.Maps;
 using Moq;
+using Tynamix.ObjectFiller;
 
 namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
 {
@@ -12,5 +16,36 @@ namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
     {
         private readonly Mock<IMapApiBroker> mapApiBrokerMock;
         private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IMapService mapService;
+
+        public MapServiceTests()
+        {
+            this.mapApiBrokerMock = new Mock<IMapApiBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.mapService = new MapService(
+                mapApiBroker: this.mapApiBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static ExternalMapSearchResponse CreateRandomExternalMapSearchResponse() =>
+            CreateRandomExternalMapSearchResponseFiller().Create();
+
+        private static Filler<ExternalMapSearchResponse> CreateRandomExternalMapSearchResponseFiller()
+        {
+            var filler = new Filler<ExternalMapSearchResponse>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset?>().Use(GetRandomDateTimeOffset());
+
+            return filler;
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+
+        private static int GetRandomNumber(int min = 1, int max = 10) =>
+            new IntRange(min, max + 1).GetValue();
     }
 }
