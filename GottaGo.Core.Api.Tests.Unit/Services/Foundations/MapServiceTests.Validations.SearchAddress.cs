3b63bb5aa3a357d1ff5907e967ae776be913da63
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-25 23:51:46 -0700

    ShouldThrowValidationExceptionOnSearchIfNullAndLogItAsync -> FAIL

---
+++ GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.Validations.SearchAddress.cs
@@ -0,0 +1,50 @@
+﻿// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GottaGo.Core.Api.Models.ExternalMaps.Search;
+using GottaGo.Core.Api.Models.Maps;
+using GottaGo.Core.Api.Models.Maps.Exceptions;
+using Moq;
+using Xunit;
+
+namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
+{
+    public partial class MapServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnSearchIfNullAndLogItAsync()
+        {
+            // given
+            AddressSearch nullAddressSearch = null;
+            var nullAddressSearchException = new NullAddressSearchException();
+            var expectedMapValidationException = new MapValidationException(nullAddressSearchException);
+
+            // when
+            ValueTask<List<Address>> searchAddressTask = this.mapService.SearchAddress(nullAddressSearch);
+
+            MapValidationException actualMapValidationException =
+                await Assert.ThrowsAsync<MapValidationException>(
+                    searchAddressTask.AsTask);
+
+            // then
+            actualMapValidationException.Should().BeEquivalentTo(expectedMapValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMapValidationException))),
+                        Times.Once);
+
+            this.mapApiBrokerMock.Verify(broker =>
+                broker.GetSearchAddressAsync(
+                    It.IsAny<ExternalMapSearchParameters>()),
+                        Times.Never);
+
+            this.mapApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ GottaGo.Core.Api/Models/Maps/Exceptions/NullAddressSearchException.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+using Xeptions;
+
+namespace GottaGo.Core.Api.Models.Maps.Exceptions
+{
+    public class NullAddressSearchException : Xeption
+    {
+        public NullAddressSearchException() :
+            base(message: "AddressSearch is null.")
+        { }
+    }
+}
