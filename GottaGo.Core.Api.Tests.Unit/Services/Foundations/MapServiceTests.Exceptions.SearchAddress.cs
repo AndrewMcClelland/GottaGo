728e06445d460ee0f1021e1b0e107ef682aa1f8b
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-25 23:18:51 -0700

    ShouldThrowDependencyExceptionOnRetrieveIfErrorOccursAndLogItAsync -> FAIL

--- GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.Exceptions.SearchAddress.cs
+++ GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.Exceptions.SearchAddress.cs
@@ -3,12 +3,14 @@
 // -----------------------------------
 
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
 using GottaGo.Core.Api.Models.ExternalMaps.Search;
 using GottaGo.Core.Api.Models.ExternalMaps.Search.Exceptions;
 using GottaGo.Core.Api.Models.Maps;
 using Moq;
+using RESTFulSense.Exceptions;
 using Xeptions;
 using Xunit;
 
@@ -43,8 +45,50 @@ namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
             actualMapDependencyException.Should().BeEquivalentTo(actualMapDependencyException);
 
             this.mapApiBrokerMock.Verify(broker =>
-                broker.GetSearchAddressAsync(It.IsAny<ExternalMapSearchParameters>()),
-                    Times.Once);
+                broker.GetSearchAddressAsync(
+                    It.IsAny<ExternalMapSearchParameters>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedMapDependencyException))),
+                        Times.Once);
+
+            this.mapApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveIfErrorOccursAndLogItAsync()
+        {
+            // given
+            var someResponseMessage = new HttpResponseMessage();
+            var someMessage = GetRandomString();
+            var httpResponseException = new HttpResponseException(someResponseMessage, someMessage);
+            var failedMapDependencyException = new FailedMapDependencyException(httpResponseException);
+            var expectedMapDependencyException = new MapDependencyException(failedMapDependencyException);
+
+            AddressSearch inputAddressSearch = CreateRandomAddressSearch();
+
+            mapApiBrokerMock.Setup(broker =>
+                broker.GetSearchAddressAsync(
+                        It.IsAny<ExternalMapSearchParameters>()))
+                            .ThrowsAsync(httpResponseException);
+
+            // when
+            ValueTask<List<Address>> searchAddressTask = this.mapService.SearchAddress(inputAddressSearch);
+
+            MapDependencyException actualMapDependencyException =
+                await Assert.ThrowsAsync<MapDependencyException>(
+                    searchAddressTask.AsTask);
+
+            // then
+            actualMapDependencyException.Should().BeEquivalentTo(actualMapDependencyException);
+
+            this.mapApiBrokerMock.Verify(broker =>
+                broker.GetSearchAddressAsync(
+                    It.IsAny<ExternalMapSearchParameters>()),
+                        Times.Once);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
