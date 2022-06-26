e215899edb4458c6c600c5fb2409e030f1c88c9e
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-25 22:40:03 -0700

    ShouldSearchAddressAsync -> FAIL

--- GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.Logic.SearchAddress.cs
+++ GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.Logic.SearchAddress.cs
@@ -2,11 +2,14 @@
 // Copyright (c) Andrew McClelland.
 // -----------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
 using GottaGo.Core.Api.Models.ExternalMaps.Search;
 using GottaGo.Core.Api.Models.Maps;
+using Moq;
 using Xunit;
 
 namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
@@ -17,18 +20,77 @@ namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
         public async Task ShouldSearchAddressAsync()
         {
             // given
-            ExternalMapSearchResponse externalMapSearchResponse = CreateRandomExternalMapSearchResponse();
+            ExternalMapSearchResponse randomExternalMapSearchResponse = CreateRandomExternalMapSearchResponse();
+            ExternalMapSearchResponse retrievedExternalMapSearchResponse = randomExternalMapSearchResponse;
 
-            List<Address> expectedAddressers = externalMapSearchResponse.Responses.Select(response =>
+            List<Address> expectedAddresses = randomExternalMapSearchResponse.Responses.Select(response =>
                 new Address
                 {
-                    
+                    Type = response.Type,
+                    StreetNumber = response.Address.StreetNumber,
+                    StreetName = response.Address.StreetName,
+                    MunicipalitySubdivision = response.Address.MunicipalitySubdivision,
+                    Municipality = response.Address.Municipality,
+                    CountrySubdivisionName = response.Address.CountrySubdivisionName,
+                    CountrySubdivision = response.Address.CountrySubdivision,
+                    CountrySecondarySubdivision = response.Address.CountrySecondarySubdivision,
+                    CountryTertiarySubdivision = response.Address.CountryTertiarySubdivision,
+                    PostalCode = response.Address.PostalCode,
+                    ExtendedPostalCode = response.Address.ExtendedPostalCode,
+                    Country = response.Address.Country,
+                    CountryCode = response.Address.CountryCode,
+                    CountryCodeISO3 = response.Address.CountryCodeISO3,
+                    FreeformAddress = response.Address.FreeformAddress,
+                    Coordinates = new Coordinates
+                    {
+                        Latitude = response.Position.Latitude,
+                        Longitude = response.Position.Longitude
+                    },
+                    BoundingBox = new BoundingBox
+                    {
+                        TopLeftPoint = new Coordinates
+                        {
+                            Latitude = response.Viewport.TopLeftPoint.Latitude,
+                            Longitude = response.Viewport.TopLeftPoint.Longitude,
+                        },
+                        BottomRightPoint = new Coordinates
+                        {
+                            Latitude = response.Viewport.BottomRightPoint.Latitude,
+                            Longitude = response.Viewport.BottomRightPoint.Longitude,
+                        },
+                    }
                 }).ToList();
 
-            // when
+            AddressSearch randomAddressSearch = CreateRandomAddressSearch();
+            AddressSearch inputAddressSearch = randomAddressSearch;
+            var expectedExternalMapSearchParameters = new ExternalMapSearchParameters
+            {
+                Query = inputAddressSearch.Query,
+                Language = inputAddressSearch.Language,
+                CountrySet = String.Join(",", inputAddressSearch.Countries),
+                Latitude = inputAddressSearch.CurrentLocation.Latitude,
+                Longitude = inputAddressSearch.CurrentLocation.Longitude,
 
+            };
+
+            this.mapApiBrokerMock.Setup(broker =>
+                broker.GetSearchAddressAsync(It.Is(
+                    SameInformationAs(expectedExternalMapSearchParameters))))
+                        .ReturnsAsync(retrievedExternalMapSearchResponse);
+
+            // when
+            List<Address> actualAddresses = await this.mapService.SearchAddress(inputAddressSearch);
 
             // then
-        }
+            actualAddresses.Should().BeEquivalentTo(expectedAddresses);
+
+            this.mapApiBrokerMock.Verify(broker =>
+                broker.GetSearchAddressAsync(It.Is(
+                    SameInformationAs(expectedExternalMapSearchParameters))),
+                        Times.Once);
+
+            this.mapApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        } 
     }
 }
--- GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.cs
+++ GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.cs
@@ -3,10 +3,13 @@
 // -----------------------------------
 
 using System;
+using System.Linq.Expressions;
 using GottaGo.Core.Api.Brokers.Loggings;
 using GottaGo.Core.Api.Brokers.MapApis;
 using GottaGo.Core.Api.Models.ExternalMaps.Search;
+using GottaGo.Core.Api.Models.Maps;
 using GottaGo.Core.Api.Services.Foundations.Maps;
+using KellermanSoftware.CompareNetObjects;
 using Moq;
 using Tynamix.ObjectFiller;
 
@@ -16,12 +19,14 @@ namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
     {
         private readonly Mock<IMapApiBroker> mapApiBrokerMock;
         private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ICompareLogic compareLogic;
         private readonly IMapService mapService;
 
         public MapServiceTests()
         {
             this.mapApiBrokerMock = new Mock<IMapApiBroker>();
             this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.compareLogic = new CompareLogic();
 
             this.mapService = new MapService(
                 mapApiBroker: this.mapApiBrokerMock.Object,
@@ -30,6 +35,26 @@ namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
 
         private static ExternalMapSearchResponse CreateRandomExternalMapSearchResponse() =>
             CreateRandomExternalMapSearchResponseFiller().Create();
+
+        private static AddressSearch CreateRandomAddressSearch() =>
+            CreateRandomAddressSearchFiller().Create();
+
+        private Expression<Func<ExternalMapSearchParameters, bool>> SameInformationAs(
+            ExternalMapSearchParameters expectedExternalMapSearchParameters)
+        {
+            return actualExternalMapSearchParameters =>
+                this.compareLogic.Compare(
+                    expectedExternalMapSearchParameters,
+                    actualExternalMapSearchParameters)
+                        .AreEqual;
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+
+        private static int GetRandomNumber(int min = 1, int max = 10) =>
+            new IntRange(min, max + 1).GetValue();
 
         private static Filler<ExternalMapSearchResponse> CreateRandomExternalMapSearchResponseFiller()
         {
@@ -41,11 +66,7 @@ namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
             return filler;
         }
 
-        private static DateTimeOffset GetRandomDateTimeOffset() =>
-            new DateTimeRange(earliestDate: new DateTime()).GetValue();
-
-
-        private static int GetRandomNumber(int min = 1, int max = 10) =>
-            new IntRange(min, max + 1).GetValue();
+        private static Filler<AddressSearch> CreateRandomAddressSearchFiller() =>
+            new Filler<AddressSearch>();
     }
 }
