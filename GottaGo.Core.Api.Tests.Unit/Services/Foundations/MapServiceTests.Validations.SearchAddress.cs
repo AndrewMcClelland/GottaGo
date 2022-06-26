6e6d1f14fbb92856aef3e23f107667802778b882
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-26 00:14:00 -0700

    ShouldThrowValidationExceptionOnSearchIfAddressIsInvalidAndLogItAsync -> FAIL

--- GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.Validations.SearchAddress.cs
+++ GottaGo.Core.Api.Tests.Unit/Services/Foundations/MapServiceTests.Validations.SearchAddress.cs
@@ -46,5 +46,50 @@ namespace GottaGo.Core.Api.Tests.Unit.Services.Foundations
             this.mapApiBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async void ShouldThrowValidationExceptionOnSearchIfAddressIsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            // given
+            var invalidAddressSearch = new AddressSearch
+            {
+                Query = invalidString
+            };
+
+            var invalidMapException = new InvalidMapException();
+
+            invalidMapException.AddData(
+                key: nameof(AddressSearch.Query),
+                values: "Query is required");
+
+            var expectedMapValidationException = new MapValidationException(invalidMapException);
+
+            // when
+            ValueTask<List<Address>> searchAddressTask = this.mapService.SearchAddress(invalidAddressSearch);
+
+            MapValidationException actualMapValidationException =
+                await Assert.ThrowsAsync<MapValidationException>(
+                    searchAddressTask.AsTask);
+
+            // then
+            actualMapValidationException.Should().BeEquivalentTo(expectedMapValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMapValidationException))),
+                        Times.Once);
+
+            this.mapApiBrokerMock.Verify(broker =>
+                broker.GetSearchAddressAsync(
+                    It.IsAny<ExternalMapSearchParameters>()),
+                        Times.Never);
+
+            this.mapApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
