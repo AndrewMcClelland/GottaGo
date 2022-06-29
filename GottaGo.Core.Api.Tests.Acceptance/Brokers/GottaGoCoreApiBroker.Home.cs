aa833dcc2b5000685ec8585fb75cd48b86ac3b01
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-28 22:12:02 -0700

    INFRA: Initialize Acceptance Tests
    Closes #5

---
+++ GottaGo.Core.Api.Tests.Acceptance/Brokers/GottaGoCoreApiBroker.Home.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------
+// Copyright (c) Andrew McClelland.
+// -----------------------------------
+
+using System.Threading.Tasks;
+
+namespace GottaGo.Core.Api.Tests.Acceptance.Brokers
+{
+    public partial class GottaGoCoreApiBroker
+    {
+        private const string HomeRelativeUrl = "api/home";
+
+        public async ValueTask<string> GetHomeMessageAsync() =>
+            await this.apiFactoryClient.GetContentStringAsync(HomeRelativeUrl);
+    }
+}
